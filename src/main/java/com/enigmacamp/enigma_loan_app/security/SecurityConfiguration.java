db4cac8f251e40c28fba9a3e0e4b2e0e606d338e
Izzulhaq Fawwaz Syauqiy <izzulhaq219@gmail.com>
2025-02-21 17:07:43 +0700

    [REFACTOR] refactor security config

--- src/main/java/com/enigmacamp/enigma_loan_app/security/SecurityConfiguration.java
+++ src/main/java/com/enigmacamp/enigma_loan_app/security/SecurityConfiguration.java
@@ -8,11 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtTokenProvider tokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -22,7 +24,10 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore();
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(tokenProvider),
+                        UsernamePasswordAuthenticationFilter.class
+                );
         return httpSecurity.build();
     }
 }
