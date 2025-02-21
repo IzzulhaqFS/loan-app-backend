794534bca02d9d870441e435e5922d5f4f30934f
Izzulhaq Fawwaz Syauqiy <izzulhaq219@gmail.com>
2025-02-21 08:20:36 +0700

    Initial Commit

---
+++ src/main/java/com/enigmacamp/enigma_loan_app/EnigmaLoanAppApplication.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.enigma_loan_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaLoanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaLoanAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigmacamp/enigma_loan_app/EnigmaLoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.enigma_loan_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaLoanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
